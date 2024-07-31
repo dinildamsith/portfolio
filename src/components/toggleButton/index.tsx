6e4d5339c160f921cee444bb2f03d07f0ba6ea58
dinildamsith <dinildamsith52@gmail.com>
2024-07-31 19:37:36 +0530

    added toggle button

---
+++ src/components/toggleButton/index.tsx
@@ -0,0 +1,21 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+function ToggleButton(props:any){
+    return(
+        <>
+            <a href="#">
+                <div className="toggleBtn relative w-[135px] bg-[#292e37] h-[46px] mt-[20px] mb-2 shadow-md ring-4 ring-transparent ring-offset-0 right-[-85px] transition-right duration-500 rounded-tl-[20px] rounded-bl-[20px] rounded-tr-none rounded-br-none hover:right-0 cursor-pointer flex items-center justify-between px-4">
+                    <div className="toggleBtnIcon text-[#63E6BE] text-xl">
+                        <i className={`${props.icon}`}></i>
+                    </div>
+                    <h3 className="toggleBtnTxt text-[#63E6BE] font-bold">
+                        {props.btnName}
+                    </h3>
+                </div>
+
+            </a>
+        </>
+    );
+}
+
+export default  ToggleButton
--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,4 +1,5 @@
 import Nav from "../../components/nav";
+import ToggleButton from "../../components/toggleButton";
 
 export default function MainPage(){
     return(
@@ -16,6 +17,13 @@ export default function MainPage(){
                         Hello! I’m Dinil, a passionate Full Stack Developer. Dive into my portfolio and explore the world of innovative solutions and creative code {"<🌍/>"}.
                     </h1>
                 </div>
+
+                <section className="toggleBtnGroup absolute right-0 top-[70px] overflow-x-hidden z-[999]">
+                    <ToggleButton btnName="Gmail" icon="fab fa-google"/>
+                    <ToggleButton btnName="Facbook" icon="fa-brands fa-facebook"/>
+                    <ToggleButton btnName="Git Hub" icon="fa-brands fa-github"/>
+                    <ToggleButton btnName="Linkdin" icon="fa-brands fa-linkedin-in"/>
+                </section>
 
             </div>
         </>
