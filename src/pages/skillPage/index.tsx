b53897545b238f7de1826e7543801c2aa232c82e
dinildamsith <dinildamsith52@gmail.com>
2024-08-02 03:16:58 +0530

    skill page added

---
+++ src/pages/skillPage/index.tsx
@@ -0,0 +1,127 @@
+import './skill.scss'
+export default function SkillPage(){
+    return(
+        <>
+            <div>
+                <h1 className="mt-52 text-white font-black-ops opacity-15 text-3xl tracking-[0.29rem] text-[50px] text-center">TECHNICAL SKILLS</h1>
+
+                <div>
+
+                    <div className="flex justify-center">
+                        <div className="p-4 flex flex-wrap gap-4 justify-center">
+                            <h1 className="text-center text-[#13f7d7] w-full mt-12 ">Languages</h1>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=java" className="w-12 h-12" alt="Java icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=python" className="w-12 h-12" alt="Python icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=html" className="w-12 h-12" alt="HTML icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=css" className="w-12 h-12" alt="CSS icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=nodejs" className="w-12 h-12" alt="Node.js icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=ts" className="w-12 h-12" alt="TypeScript icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=js" className="w-12 h-12" alt="JavaScript icon"/>
+                            </a>
+
+                        </div>
+                    </div>
+
+
+
+                    <div className="flex justify-center">
+                        <div className="p-4 flex flex-wrap gap-4 justify-center">
+                            <h1 className="text-center text-[#13f7d7] w-full mb-4">Frameworks & Libraries</h1>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=spring" className="w-12 h-12" alt="Java icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=express" className="w-12 h-12" alt="Python icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=react" className="w-12 h-12" alt="HTML icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=hibernate" className="w-12 h-12" alt="JavaScript icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=tailwind" className="w-12 h-12" alt="CSS icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=bootstrap" className="w-12 h-12" alt="Node.js icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=npm" className="w-12 h-12" alt="TypeScript icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=jquery" className="w-12 h-12" alt="JavaScript icon"/>
+                            </a>
+
+                        </div>
+                    </div>
+
+
+                    <div className="flex justify-center">
+                        <div className="p-4 flex flex-wrap gap-4 justify-center">
+                            <h1 className="text-center text-[#13f7d7] w-full mb-4">Database & Tools</h1>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=mysql" className="w-12 h-12" alt="Java icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=mongodb" className="w-12 h-12" alt="Python icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=maven" className="w-12 h-12" alt="HTML icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=gradle" className="w-12 h-12" alt="JavaScript icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=vite" className="w-12 h-12" alt="JavaScript icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=firebase" className="w-12 h-12" alt="JavaScript icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=figma" className="w-12 h-12" alt="JavaScript icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=idea" className="w-12 h-12" alt="Node.js icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=vscode" className="w-12 h-12" alt="TypeScript icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=postman" className="w-12 h-12" alt="CSS icon"/>
+                            </a>
+
+                        </div>
+                    </div>
+
+
+                    <div className="flex justify-center">
+                        <div className="p-4 flex flex-wrap gap-4 justify-center">
+                            <h1 className="text-center text-[#13f7d7] w-full mb-4">Version Controller</h1>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=git" className="w-12 h-12 " alt="Java icon"/>
+                            </a>
+                            <a href="">
+                                <img src="https://skillicons.dev/icons?i=github" className="w-12 h-12" alt="Python icon"/>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
