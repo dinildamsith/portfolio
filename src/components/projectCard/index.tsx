7598607627d7e113682dbacf1b0cd75b7d2caaff
dinildamsith <dinildamsith52@gmail.com>
2024-08-02 21:57:43 +0530

    project page added

--- src/components/projectCard/index.tsx
+++ src/components/projectCard/index.tsx
@@ -21,10 +21,7 @@ export function ProjectCard(props:any) {
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                         {props.desc}
                     </p>
-                    <a
-                        href={props.repoUrl}
-                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
+                    <a href={props.repoUrl} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Github
                         <svg
                             className="w-5 h-5 ml-2"
--- src/pages/projectPage/index.tsx
+++ src/pages/projectPage/index.tsx
@@ -11,7 +11,14 @@ export function ProjectPage() {
 
                 <div className="flex justify-center mt-10">
                     <div className="p-4 flex flex-wrap gap-4 justify-center">
-                        <ProjectCard img='src/assets/imges/test.jpeg' name='ff' desc='lorempsgksrkgoijgijgirj' repoUrl='https://github.com/dinildamsith/react'/>
+                        <ProjectCard img='src/assets/imges/meat.jpg' name='Hen House System' desc={''} repoUrl='https://github.com/dinildamsith/HenHouse_Layerd_Architecture'/>
+                        <ProjectCard img='src/assets/imges/conect4.jpg' name='Connect 4 Game' desc='' repoUrl='https://github.com/dinildamsith/Connect_4_Game'/>
+                        <ProjectCard img='src/assets/imges/hostel.jpg' name='Hostel Managment System' desc='' repoUrl='https://github.com/dinildamsith/D24_Hostel_Managment_System_Hibernate'/>
+                        <ProjectCard img='src/assets/imges/gmail.jpg' name='Gmail Mobile App Clone' desc='' repoUrl='https://github.com/dinildamsith/Gmail_App_Clone'/>
+                        <ProjectCard img='src/assets/imges/ticket.jpg' name='Highway Ticket System' desc='' repoUrl='https://github.com/dinildamsith/Highway_Ticket_ManagementSystem_Microservice'/>
+                        <ProjectCard img='src/assets/imges/shoes.jpg' name='Shoes Shop System' desc='' repoUrl='https://github.com/dinildamsith/HelloShoesShop'/>
+                        <ProjectCard img='src/assets/imges/pos.jpg' name='POS System' desc='' repoUrl='https://github.com/dinildamsith/PosSystem'/>
+                        <ProjectCard img='src/assets/imges/chatApp.jpg' name='Chat App' desc='' repoUrl='https://github.com/dinildamsith/Chat_App'/>
                     </div>
                 </div>
 
