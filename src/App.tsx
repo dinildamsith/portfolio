e5d752eda92f5a6490ed4e7984b1815fd5325ef4
dinildamsith <dinildamsith52@gmail.com>
2024-08-03 04:02:03 +0530

    config

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<MainPage/>} />
+                <Route path="/portfolioReact" element={<MainPage/>} />
             </Routes>
         </BrowserRouter>
     </>
