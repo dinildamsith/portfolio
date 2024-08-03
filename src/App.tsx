694f2aab66c47042cd00df219a8ce856a0eb7b77
dinildamsith <dinildamsith52@gmail.com>
2024-08-03 11:50:46 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import './App.css'
-// import MainPage from "./pages/mainPage";
-// import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainPage from "./pages/mainPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
 
   return (
     <>
-        <h1 className="text-white">gii</h1>
-        {/*<BrowserRouter>*/}
-        {/*    <Routes>*/}
-        {/*        <Route path="/" element={<MainPage/>} />*/}
-        {/*    </Routes>*/}
-        {/*</BrowserRouter>*/}
+        <BrowserRouter>
+            <Routes>
+                <Route path="/portfolioReact/" element={<MainPage/>} />
+            </Routes>
+        </BrowserRouter>
     </>
   )
 }
